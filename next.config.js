573bd4dc462065eb594bc60bd52c213fe1775718
Kian A <kian@assary.io>
2025-11-16 20:01:57 -0500

    m

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: ['placeholder.com', 'vercel.blob.store'],
   },
   eslint: {
-    ignoreDuringBuilds: false,
+    ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: false,
